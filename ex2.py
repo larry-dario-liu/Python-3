0f0a7504404df57640ceafb948e20fcf72abbf85
larry-dario-liu <larry-dario-liu@qq.com>
2017-08-14 11:28:20 +0800

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,15 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("hens",25+30/6,5>=2)
+
+hens 30.0 True
+>>> print ("hens%",25+30/6,5>=2,sep="\n")
+hens%
+30.0
+True
+>>> num = 50
+>>> print ("hens  %s"%num,25+30/6,5>=2,sep="\n")
+hens  50
+30.0
+True
+>>> 
---
+++ ex3.py
@@ -0,0 +1,11 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> age =input()
+60
+>>> age
+'60'
+>>> age = int(input())
+60
+>>> age
+60
+>>> 
