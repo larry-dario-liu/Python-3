6aebfa7d8429e0c78a3b1f12e9b87d2beaa86762
larry-dario-liu <larry-dario-liu@qq.com>
2017-08-14 11:17:54 +0800

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,12 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("I will show you the difference","not much","fine","\n")
+I will show you the difference not much fine 
+
+>>> print ("I will show you the difference","not much","fine","/n")
+I will show you the difference not much fine /n
+>>> print ("I will show you the difference","not much","fine",sep="\n")
+I will show you the difference
+not much
+fine
+>>> 
